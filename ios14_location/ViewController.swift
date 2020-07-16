4af54d3dc867649d91a7547f95bb743561570042
koji-torishima <torikou3685@gmail.com>
2020-07-16 20:18:54 +0900

    Initial Commit

---
+++ ios14_location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios14_location
+//
+//  Created by koji torishima on 2020/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
